3043405d8730c67b3bb2871aa217c4428c3341e2
John Demetrio <johndemetrio123@gmail.com>
2024-01-17 12:28:38 -0300

    ep 07

---
+++ 03 - Listas e loops no C#/DesafioAlura/Program.cs
@@ -0,0 +1,11 @@
+﻿void funcaoOperacoesBasicas(float a, float b){
+    float soma = a + b;
+    float subtracao = a - b;
+    float divisao = a / b;
+    float multiplicacao = a * b;
+    Console.WriteLine("A soma do número A:{0} e do B:{1} é de {2}", a, b, soma);
+    Console.WriteLine("A subtração do número A:{0} e do B:{1} é de {2}", a, b, subtracao);
+    Console.WriteLine("A divisão do número A:{0} e do B:{1} é de {2}", a, b, divisao);
+    Console.WriteLine("A multiplicação do número A:{0} e do B:{1} é de {2}", a, b, multiplicacao);
+}
+funcaoOperacoesBasicas(10, 2);
