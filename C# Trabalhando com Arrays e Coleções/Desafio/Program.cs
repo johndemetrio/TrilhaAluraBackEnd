80412f6dfd4349d4263c56a6d0d64128a9e98e37
John Demetrio <johndemetrio123@gmail.com>
2024-01-26 23:59:45 -0300

    add: desafio 1

---
+++ C# Trabalhando com Arrays e Coleções/Desafio/Program.cs
@@ -0,0 +1,11 @@
+﻿void Menu()
+{
+    Console.Writeline("Monte seu time")
+    Console.Writeline("1 - Escolha a formação do time")
+    Console.Writeline("2 - Busque o seu time")
+}
+char opcao = '0';
+while( opcao != 6)
+{
+
+}
