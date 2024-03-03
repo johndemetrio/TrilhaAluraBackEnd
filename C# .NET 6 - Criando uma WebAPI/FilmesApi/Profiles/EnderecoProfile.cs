2619ec4f67cdbb7e949b310e2a50860975cc0cd8
John Demetrio <johndemetrio123@gmail.com>
2024-03-03 10:51:24 -0300

    add: conteudo do curso sobre api com .net 6

---
+++ C# .NET 6 - Criando uma WebAPI/FilmesApi/Profiles/EnderecoProfile.cs
@@ -0,0 +1,14 @@
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+using AutoMapper;
+namespace FilmesApi.Profiles;
+
+public class EnderecoProfile : Profile
+{
+    public EnderecoProfile()
+    {
+        CreateMap<CreateEnderecoDto, Endereco>();
+        CreateMap<Endereco, ReadEnderecoDto>();
+        CreateMap<UpdateEnderecoDto, Endereco>();  
+    }
+}
